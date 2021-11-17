ead11c6e6e92c26d932e64ac49f5a4854bcc4aab
asanjabi <asanjabi@users.noreply.github.com>
2021-11-17 09:25:54 -0800

    setting up 4

---
+++ challenges/4/2-deployall.sh
@@ -0,0 +1,5 @@
+kubectl apply -f poi-deployment.yml
+kubectl apply -f trip-viewer-deployment.yml
+kubectl apply -f trips-deployment.yml
+kubectl apply -f user-java-deployment.yml
+kubectl apply -f user-profile-deployment.yml
---
+++ challenges/4/removeall.sh
@@ -0,0 +1,6 @@
+kubectl delete -f poi-deployment.yml
+kubectl delete -f trip-viewer-deployment.yml
+kubectl delete -f trips-deployment.yml
+kubectl delete -f user-java-deployment.yml
+kubectl delete -f user-profile-deployment.yml
+kubectl delete -f trip-viewer-service.yml
