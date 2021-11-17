b2bdcdfe8d18f7d7677d3292044dd943866606ef
asanjabi <asanjabi@users.noreply.github.com>
2021-11-17 07:29:35 -0800

    added delete script

--- challenges/3/2-deployall.sh
+++ challenges/3/2-deployall.sh
@@ -3,4 +3,3 @@ kubectl apply -f trip-viewer-deployment.yml
 kubectl apply -f trips-deployment.yml
 kubectl apply -f user-java-deployment.yml
 kubectl apply -f user-profile-deployment.yml
-kubectl apply -f trip-viewer-service.yml
